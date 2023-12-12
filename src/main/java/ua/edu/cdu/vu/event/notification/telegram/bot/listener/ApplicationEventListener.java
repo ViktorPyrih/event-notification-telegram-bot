db8afa69b12247e4a775147ec3ba91dae2a0eabc
ViktorPyrih <v24102002>
2023-12-12 02:09:08 +0200

    Updated ApplicationEventListener event type

--- src/main/java/ua/edu/cdu/vu/event/notification/telegram/bot/listener/ApplicationEventListener.java
+++ src/main/java/ua/edu/cdu/vu/event/notification/telegram/bot/listener/ApplicationEventListener.java
@@ -1,7 +1,7 @@
 package ua.edu.cdu.vu.event.notification.telegram.bot.listener;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -15,7 +15,7 @@ public class ApplicationEventListener {
     private final TelegramBotsApi api;
     private final EventNotificationTelegramBotApplication.EventNotificationTelegramBot bot;
 
-    @EventListener(ContextRefreshedEvent.class)
+    @EventListener(ApplicationReadyEvent.class)
     public void onApplicationStartup() throws TelegramApiException {
         api.registerBot(bot);
     }
